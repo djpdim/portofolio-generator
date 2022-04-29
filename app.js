abfb83c2edd69e17da083e6529d0a92651946a88
djpdim <dimospagkas@yahoo.gr>
2022-04-28 20:52:08 -0400

    update files

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+.prompt([
+  {
+    type:'input',
+    name:'name',
+    message:'What is your name?'
   }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+])
+.then(answers => sonsole.log(answers));
